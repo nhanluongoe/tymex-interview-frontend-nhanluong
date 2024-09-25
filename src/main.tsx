7c40cd74c45c021d9ca31172601a4bc139065780
nhanluongoe <nhanlt18@gmail.com>
2024-09-25 16:45:20 +0700

    chore: setup axios, react-query and msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,20 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@routes/index.tsx";
 import "./styles/index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@libs/react-query.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser.ts");
+  return worker.start();
+}
+
+enableMocking().then(() =>
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  )
 );

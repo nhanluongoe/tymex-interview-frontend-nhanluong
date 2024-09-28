2271d9180bae384e443bd30b3e5fd8133e4fcda0
nhanluongoe <nhanlt18@gmail.com>
2024-09-28 22:11:29 +0700

    fix: responsiveness on tabs

--- src/modules/marketplace/pages/index.tsx
+++ src/modules/marketplace/pages/index.tsx
@@ -1,10 +1,9 @@
 import Button from "@components/ui/Button";
 import Tabs from "@components/ui/Tabs";
 import useBreakpoints from "@hooks/useBreakpoints";
-import useWindowDimensions from "@hooks/useWindowDimension";
 import { FilterAlt } from "@mui/icons-material";
 import { Box, Drawer, Grid2 } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Banner from "../assets/images/banner.png";
 import LightLines from "../assets/images/light-lines.png";
 import TokenCard from "../components/TokenCard";
@@ -15,28 +14,6 @@ export default function MarketPlacePage() {
   const { tokens, fetchNextPage } = useToken();
   const { isTablet } = useBreakpoints();
   const [showFiltersOnTablet, setShowFiltersOnTablet] = useState(false);
-
-  const leftSectionRef = useRef<HTMLDivElement>(null);
-  const [rightSectionWidth, setRightSectionWidth] = useState(0);
-  const { width: windowWidth } = useWindowDimensions();
-
-  useEffect(() => {
-    const leftSectionWidth = leftSectionRef.current?.clientWidth || 0;
-
-    const handleResize = () => {
-      const viewWidth = windowWidth;
-      const offset = isTablet ? 8 : 56;
-      const newRightSectionWidth = viewWidth - leftSectionWidth - offset * 2;
-      setRightSectionWidth(newRightSectionWidth);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [windowWidth, isTablet]);
 
   const handleToggleFilters = (newOpen: boolean) => () => {
     setShowFiltersOnTablet(newOpen);
@@ -56,7 +33,6 @@ export default function MarketPlacePage() {
       <Box
         sx={{
           backgroundImage: "url(/background.png)",
-          backgroundPosition: "auto",
           pt: isTablet ? 2 : 8,
         }}
       >
@@ -81,20 +57,21 @@ export default function MarketPlacePage() {
           </Box>
         )}
 
-        <Box
+        <Grid2
+          container
           sx={{
             px: isTablet ? 1 : 7,
-            display: "flex",
-            maxWidth: "100%",
+            // display: "flex",
+            // maxWidth: "100%",
           }}
         >
           {!isTablet && (
-            <Box ref={leftSectionRef} sx={{ minWidth: "25%" }}>
+            <Grid2 size={4}>
               <TokenFilters />
-            </Box>
+            </Grid2>
           )}
-          <Box sx={{ mb: 8, flexGrow: 1 }}>
-            <Box sx={{ width: `${rightSectionWidth}px`, my: 3 }}>
+          <Grid2 sx={{ mb: 8, flexGrow: 1 }} size={isTablet ? 12 : 8}>
+            <Box sx={{ maxWidth: "100%", my: 3 }}>
               <Tabs />
             </Box>
             <Grid2 container spacing={2} columns={12}>
@@ -117,8 +94,8 @@ export default function MarketPlacePage() {
                 View More
               </Button>
             </Box>
-          </Box>
-        </Box>
+          </Grid2>
+        </Grid2>
         <Box component="img" src={LightLines} sx={{ width: "100%" }}></Box>
       </Box>
     </Box>

e5f1c5b3189eeb50f1b26e23e326be92d6e829af
nhanluongoe <nhanlt18@gmail.com>
2024-09-29 23:17:58 +0700

    feat: add ui for home page

--- src/modules/home/pages/index.tsx
+++ src/modules/home/pages/index.tsx
@@ -1,3 +1,30 @@
+import { Box, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
 export default function HomePage() {
-    return <div>HomePage</div>
+    const navigate = useNavigate()
+
+    const handleNavigate = () => {
+        navigate('/marketplace')
+    }
+
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="70vh"
+            textAlign="center"
+        >
+            <h1>Welcome to TymeX interview assignment</h1>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNavigate}
+            >
+                Go to Marketplace
+            </Button>
+        </Box>
+    )
 }

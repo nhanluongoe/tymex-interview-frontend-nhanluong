ff0c2439a28820400fe5dc0577b6f58728cd555e
nhanluongoe <nhanlt18@gmail.com>
2024-09-29 19:31:54 +0700

    chore(config): turn off refetch on window focus and set stale time 60s

--- src/libs/react-query.ts
+++ src/libs/react-query.ts
@@ -1,5 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60, // 60 seconds
+    },
+  }
+});
 
 export default queryClient;

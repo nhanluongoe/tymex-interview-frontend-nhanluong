007704c1d6c23b993938deb223f599d833aa2036
nhanluongoe <nhanlt18@gmail.com>
2024-09-29 18:41:07 +0700

    chore(test): test custom hooks

---
+++ src/hooks/useBreakpoints.test.ts
@@ -0,0 +1,82 @@
+import { renderHook } from "@testing-library/react-hooks";
+import useBreakpoints from "./useBreakpoints";
+import { useMediaQuery } from "@mui/material";
+import { describe, it, expect, vi, Mock } from "vitest";
+
+vi.mock("@mui/material", () => ({
+  useMediaQuery: vi.fn(),
+}));
+
+describe("useBreakpoints", () => {
+  it("should return isMobile as true when max-width is 600px", () => {
+    (useMediaQuery as Mock).mockImplementation(
+      (query) => query === "(max-width: 600px)"
+    );
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(true);
+    expect(result.current.isTablet).toBe(false);
+    expect(result.current.isDesktop).toBe(false);
+  });
+
+  it("should return isTablet as true when max-width is 960px", () => {
+    (useMediaQuery as Mock).mockImplementation(
+      (query) => query === "(max-width: 960px)"
+    );
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(false);
+    expect(result.current.isTablet).toBe(true);
+    expect(result.current.isDesktop).toBe(false);
+  });
+
+  it("should return isDesktop as true when min-width is 960px", () => {
+    (useMediaQuery as Mock).mockImplementation(
+      (query) => query === "(min-width: 960px)"
+    );
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(false);
+    expect(result.current.isTablet).toBe(false);
+    expect(result.current.isDesktop).toBe(true);
+  });
+
+  it("should return all false when no media query matches", () => {
+    (useMediaQuery as Mock).mockImplementation(() => false);
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(false);
+    expect(result.current.isTablet).toBe(false);
+    expect(result.current.isDesktop).toBe(false);
+  });
+
+  it("should return isMobile and isTablet as true when max-width is 960px and max-width is 600px", () => {
+    (useMediaQuery as Mock).mockImplementation(
+      (query) =>
+        query === "(max-width: 600px)" || query === "(max-width: 960px)"
+    );
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(true);
+    expect(result.current.isTablet).toBe(true);
+    expect(result.current.isDesktop).toBe(false);
+  });
+
+  it("should return isTablet and isDesktop as true when min-width is 960px and max-width is 960px", () => {
+    (useMediaQuery as Mock).mockImplementation(
+      (query) =>
+        query === "(min-width: 960px)" || query === "(max-width: 960px)"
+    );
+
+    const { result } = renderHook(() => useBreakpoints());
+
+    expect(result.current.isMobile).toBe(false);
+    expect(result.current.isTablet).toBe(true);
+    expect(result.current.isDesktop).toBe(true);
+  });
+});
---
+++ src/hooks/useWindowDimension.test.ts
@@ -0,0 +1,33 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useWindowDimensions from "./useWindowDimension";
+import { describe, expect, it } from "vitest";
+
+describe("useWindowDimensions", () => {
+  it("should return initial window dimensions", () => {
+    const { result } = renderHook(() => useWindowDimensions());
+    expect(result.current).toEqual({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  });
+
+  it("should update dimensions on window resize", () => {
+    const { result } = renderHook(() => useWindowDimensions());
+
+    act(() => {
+      window.innerWidth = 500;
+      window.innerHeight = 500;
+      window.dispatchEvent(new Event("resize"));
+    });
+
+    expect(result.current).toEqual({ width: 500, height: 500 });
+
+    act(() => {
+      window.innerWidth = 800;
+      window.innerHeight = 600;
+      window.dispatchEvent(new Event("resize"));
+    });
+
+    expect(result.current).toEqual({ width: 800, height: 600 });
+  });
+});
